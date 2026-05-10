48ce1efd696e8e22a326f3305d524d76a32d6b7c
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2026-05-10 12:57:17 -0400

    release: 0.6.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Uniq.MixProject do
   def project do
     [
       app: :uniq,
-      version: "0.6.2",
+      version: "0.6.3",
       elixir: "~> 1.13",
       description: description(),
       package: package(),
